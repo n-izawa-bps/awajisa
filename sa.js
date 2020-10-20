041a96fec104ab51f779d09519e46952827dfa56
n-izawa <n-izawa@bps21.jp>
2020-10-20 11:54:25 +0900

    fix: Class削除。jquery導入。invalidの数でエラー判定。

--- sa.js
+++ sa.js
@@ -81,107 +81,93 @@ function entryChange4(){
 	}
 }
 
-class ErrorInfo {
-	constructor() {
-		this.is_error = false;
-		this.first_error_top = 0;
+function checkRequire(target) {
+	let parent = target.parent();
+	let title_box = parent.prev();
+
+	if (target.val() != "") {
+		title_box.removeClass("invalid");
+		return true;
 	}
 
-	checkRequire(target) {
-		// title-boxのinvalidを削除
-		let parent = target.parentElement;
-		let title_box = parent.previousElementSibling;
+	title_box.addClass("invalid");
+}
 
-		if (target.value != "") {
-			title_box.classList.remove("invalid");
+function checkRequireForCheckbox(target) {
+	let parent = target.parent();
+	let title_box = parent.prev();
+
+	let target_array = target.children();
+	for (let checkbox of target_array) {
+		if (checkbox.checked) {
+			title_box.removeClass("invalid");
 			return true;
 		}
-
-		title_box.classList.add("invalid");
-		if (this.first_error_top == 0) {
-			this.first_error_top = title_box.getBoundingClientRect().top + window.pageYOffset;
-		}
-		this.is_error = true;
 	}
 
-	checkRequireForCheckbox(target) {
-		// title-boxのinvalidを削除
-		let parent = target.parentElement;
-		let title_box = parent.previousElementSibling;
-
-		let target_array = target.children;
-		for (let checkbox of target_array) {
-			if (checkbox.checked) {
-				title_box.classList.remove("invalid");
-				return true;
-			}
-		}
-
-		title_box.classList.add("invalid");
-		if (this.first_error_top == 0) {
-			this.first_error_top = title_box.getBoundingClientRect().top + window.pageYOffset;
-		}
-		this.is_error = true;
-	}
+	title_box.addClass("invalid");
 }
 
 // 送信前必須チェック
 function check() {
-	// エラークラス生成
-	let errorObj = new ErrorInfo();
+	$('.invalid').each(function() {
+		$(this).removeClass("invalid");
+	});
 
 	// 必須項目
-	const gender = document.getElementById("gender");
-	const age = document.getElementById("age");
-	const address_level2 = document.getElementById("address-level2");
-	const transportation = document.getElementById("transportation");
-	const smart_ic = document.getElementById("smart-ic");
-	const purpose2 = document.getElementById("purpose2");
-	const course = document.getElementById("course");
-	const highway = document.getElementById("highway");
-	const reason = document.getElementById("reason");
-	const sa = document.getElementById("sa");
-	const purpose = document.getElementById("purpose");
-	const shop = document.getElementById("shop");
-	const timeZone = document.getElementById("timeZone");
-	const staying_time = document.getElementById("staying-time");
-	const oasis = document.getElementById("oasis");
-	const purpose3 = document.getElementById("purpose3");
-	const facility = document.getElementById("facility");
+	const gender = $('#gender');
+	const age = $("#age");
+	const address_level2 = $("#address-level2");
+	const transportation = $("#transportation");
+	const smart_ic = $("#smart-ic");
+	const purpose2 = $("#purpose2");
+	const course = $("#course");
+	const highway = $("#highway");
+	const reason = $("#reason");
+	const sa = $("#sa");
+	const purpose = $("#purpose");
+	const shop = $("#shop");
+	const timeZone = $("#timeZone");
+	const staying_time = $("#staying-time");
+	const oasis = $("#oasis");
+	const purpose3 = $("#purpose3");
+	const facility = $("#facility");
 
 	// 必須チェック
-	errorObj.checkRequire(gender);
-	errorObj.checkRequire(age);
-	errorObj.checkRequire(address_level2);
-	errorObj.checkRequire(transportation);
-	errorObj.checkRequire(smart_ic);
+	checkRequire(gender);
+	checkRequire(age);
+	checkRequire(address_level2);
+	checkRequire(transportation);
+	checkRequire(smart_ic);
 
-	if (smart_ic.value == "" || smart_ic.value == "yes") {
-		errorObj.checkRequire(purpose2);
+	if (smart_ic.val() == "" || smart_ic.val() == "yes") {
+		checkRequire(purpose2);
 
-		if (purpose2.value == "" || purpose2.value == "1") {
-			errorObj.checkRequireForCheckbox(course);
+		if (purpose2.val() == "" || purpose2.val() == "1") {
+			checkRequireForCheckbox(course);
 		}
 
-		errorObj.checkRequire(highway);
-		errorObj.checkRequireForCheckbox(reason);
+		checkRequire(highway);
+		checkRequireForCheckbox(reason);
 	}
 
-		errorObj.checkRequire(sa);
-		errorObj.checkRequireForCheckbox(purpose);
-		errorObj.checkRequireForCheckbox(shop);
-		errorObj.checkRequire(timeZone);
-		errorObj.checkRequire(staying_time);
-		errorObj.checkRequire(oasis);
+		checkRequire(sa);
+		checkRequireForCheckbox(purpose);
+		checkRequireForCheckbox(shop);
+		checkRequire(timeZone);
+		checkRequire(staying_time);
+		checkRequire(oasis);
 
-	if (oasis.value == "" || oasis.value == "yes") {
-		errorObj.checkRequireForCheckbox(purpose3);
-		errorObj.checkRequireForCheckbox(facility);
+	if (oasis.val() == "" || oasis.val() == "yes") {
+		checkRequireForCheckbox(purpose3);
+		checkRequireForCheckbox(facility);
 	}
 
 	// 最終チェック
-	if (errorObj.is_error) {
-		document.documentElement.scrollTop = errorObj.first_error_top;
+	if ($('.invalid').length) {
+		let top = $('.invalid').first().offset().top;
+		$(window).scrollTop(top);
+
 		alert("必須項目を入力してください");
 		return false;
 	}
