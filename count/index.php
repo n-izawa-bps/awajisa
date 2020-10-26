4322b9f20b71382a411b3982e5cbfa3b6263bc97
n-izawa <n-izawa@bps21.jp>
2020-10-26 09:16:11 +0900

    fix: カウントページにhtmlを追加

--- count/index.php
+++ count/index.php
@@ -6,9 +6,18 @@
     $files_up = glob($dir_up . '{*.csv}', GLOB_BRACE);
     $files_dwn = glob($dir_dwn . '{*.csv}', GLOB_BRACE);
     $files_etc = glob($dir_etc . '{*.csv}', GLOB_BRACE);
-
-    echo "アンケート回答数<br>";
-    echo "上り線：" . count($files_up) . "件<br>";
-    echo "下り線：" . count($files_dwn) . "件<br>";
-    echo "不明：" . count($files_etc) . "件<br>";
 ?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>回答数カウント</title>
+</head>
+<body>
+    <h1>アンケート回答数</h1>
+    <p>上り線：<?php echo count($files_up) ?>件<br></p>
+    <p>下り線：<?php echo count($files_dwn) ?>件<br></p>
+    <p>不明：<?php echo count($files_etc) ?>件<br></p>
+</body>
+</html>
