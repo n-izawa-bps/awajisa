7c49fb407891fd067caf683fba0e104add57a9af
n-izawa <n-izawa@bps21.jp>
2020-10-30 14:14:15 +0900

    free版差し替え、追加

---
+++ free/function.php
@@ -0,0 +1,245 @@
+<?php
+require "config.php";
+date_default_timezone_set('Asia/Tokyo');
+
+// ランダムな英数字を生成
+function createRandomString($length = 4)
+{
+    $result = "";
+    $str = array_merge(range('a', 'z'), range('0', '9'));
+
+    for ($i = 0; $i < $length; $i++) {
+        $result .= $str[rand(0, count($str) - 1)];
+    }
+
+    return $result;
+}
+
+// CSVデータを取得
+function getCsvData($key)
+{
+    // 個別処理
+    if ($key == "user_agent") {
+        return $_SERVER['HTTP_USER_AGENT'] ?? "";
+    }
+
+    if ($key == "p") {
+        if (!isset($_GET[$key])) {
+            return "";
+        }
+
+        return $_GET[$key];
+    }
+
+    if ($key == "date") {
+        return date('Y-m-d');
+    }
+
+    if ($key == "time") {
+        return date('H:i:s');
+    }
+
+    // POSTデータ処理
+    if (!isset($_POST[$key])) {
+        return "";
+    }
+
+    if (is_array($_POST[$key])) {
+        $tmp = "\"" . implode(",", $_POST[$key]) . "\"";
+        return $tmp;
+    }
+
+    return $_POST[$key];
+}
+
+// パラメータから出力フォルダ名を取得
+function getOutputName($p, $is_open)
+{
+    if ($p == "up") {
+        if ($is_open) {
+            return "up/open";
+        }
+
+        return "up/close";
+    }
+
+    if ($p == "dwn") {
+        if ($is_open) {
+            return "dwn/open";
+        }
+
+        return "dwn/close";
+    }
+
+    return "etc";
+}
+
+// CSVファイル出力
+function exportCsv($row, $file_option, $is_open)
+{
+    $file_path = getcwd() . "/out/" . getOutputName($_GET["p"], $is_open) . "/" . $file_option . ".csv";
+    $file = fopen($file_path, "w");
+
+    // ヘッダーセット
+    $header = array_values(HEADER);
+    $header = mb_convert_encoding($header, "UTF-8");
+    fputcsv($file, $header);
+
+    // データセット
+    $row = mb_convert_encoding($row, "UTF-8");
+    fputcsv($file, $row);
+
+    fclose($file);
+}
+
+// JSONファイル出力
+function exportJson($file_option)
+{
+    $dir_name = getcwd() . "/json/" . date("Ymd") . "/";
+    if (!is_dir($dir_name)) {
+        mkdir($dir_name, 0777, true);
+    }
+
+    $file = fopen($dir_name . $file_option . ".txt", "w");
+    fwrite($file, json_encode($_POST, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+    fwrite($file, json_encode($_GET, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+    fwrite($file, json_encode($_SERVER['HTTP_USER_AGENT'], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
+    fclose($file);
+}
+
+// アンケート表示判定
+function isStartSurvey($date)
+{
+    if (strtotime($date) < strtotime(SURVEY_PRE)) {
+        return BEFORE;
+    }
+
+    if (strtotime($date) < strtotime(SURVEY_START)) {
+        return BEFORE_PRE;
+    }
+
+    if (strtotime($date) >= strtotime(SURVEY_END)) {
+        return AFTER;
+    }
+
+    return NOW;
+}
+
+// 場所取得
+function getPlace($p)
+{
+    if ($p == 'up') {
+        return "上り線";
+    }
+
+    if ($p == 'dwn') {
+        return "下り線";
+    }
+
+    return "－";
+}
+
+// 現在のSAを取得
+function getNowSA($p)
+{
+    if ($p == 'up') {
+        return "上り";
+    }
+
+    if ($p == 'dwn') {
+        return "下り";
+    }
+
+    return "";
+}
+
+// 現在と反対のSAを取得
+function getReverseSA($p)
+{
+    if ($p == 'up') {
+        return "下り";
+    }
+
+    if ($p == 'dwn') {
+        return "上り";
+    }
+
+    return "";
+}
+
+// 現在のSAの店名一覧を取得
+function getShops($p)
+{
+    if ($p == 'up') {
+        return UP_SA_SHOPS;
+    }
+
+    if ($p == 'dwn') {
+        return DWN_SA_SHOPS;
+    }
+
+    return array();
+}
+
+// 上りSAの終了時間取得
+function getEndTimeOfUp($date)
+{
+	$day = date('Y-m-d', strtotime($date));
+	$day_type = date('w', strtotime($date));
+
+	// 祝日判定
+	foreach (HOLIDAYS as $holiday) {
+		if (strtotime($day) == strtotime($holiday)) {
+			return UP_CLOSE_HOLIDAYS;
+		}
+	}
+
+	// 土日判定
+	if ($day_type == 0 || $day_type == 6) {
+		return UP_CLOSE_HOLIDAYS;
+	}
+
+	// 平日
+	return UP_CLOSE_WEEKDAYS;
+}
+
+// 営業時間内判定
+function isJudgeOpen($time_s, $time_e, $date)
+{
+	$day = date('Y-m-d', strtotime($date));
+
+	if (strtotime($date) >= strtotime($day . $time_s) && strtotime($date) < strtotime($day . $time_e)) {
+		return true;
+	}
+
+	return false;
+}
+
+// 進呈メッセージ表示判定
+function isShowPresentMessage($p, $date)
+{
+	if ($p == 'up') {
+		return isJudgeOpen(UP_OPEN, getEndTimeOfUp($date), $date);
+	}
+
+	if ($p == 'dwn') {
+		return isJudgeOpen(DWN_OPEN, DWN_CLOSE, $date);
+	}
+
+	return false;
+}
+
+// パラメータチェック
+function checkParameter($p)
+{
+    if ($p == 'up') {
+        return true;
+    }
+
+    if ($p == 'dwn') {
+        return true;
+    }
+
+    return false;
+}
+?>
---
+++ free/thanks.php
@@ -0,0 +1,91 @@
+<?php
+require "function.php";
+
+// 回答判定
+// if (!isset($_COOKIE['answered'])) {
+// 	header("location: .");
+// 	exit();
+// }
+
+// 初回表示判定
+$first_show = true;
+// if (!isset($_COOKIE['shown_thanks'])) {
+// 	$first_show = true;
+// }
+
+// 粗品メッセージ表示状態取得
+$is_survey_state = NOW;
+$is_show_present_message = true;
+
+// 粗品配布状態取得
+$present_state = 1;
+
+// cokkie
+// setcookie('shown_thanks', 1, strtotime(date("Y-m-d") . "+1 days"));
+?><!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <title>淡路サービスエリアに関するＷＥＢアンケート</title>
+    <meta name="description" content="淡路サービスエリアに関するＷＥＢアンケート">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script type="text/javascript" src="sa.js"></script>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-EGSLEG16QP"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-EGSLEG16QP');
+    </script>
+</head>
+
+<body onload="checkTransition()">
+	<div class="main_bk">
+        <p class="mt-3"><img class="img-header" src="./img/jbh4.png" alt="JB本四高速"></p>
+		<?php if ($first_show) : ?>
+			<div id="first">
+				<div class="d-flex justify-content-center align-items-center title">
+					<h1 class="p-3">淡路サービスエリア等に関する<br>ＷＥＢアンケート（<?= getPlace($_GET['p']) ?>）</h1>
+					<p class="d-block"><img class="img-logo" src="./img/wataru.png" alt="わたる君"></p>
+				</div>
+				<h1 class="pt-4 title">
+					アンケートにご協力いただきありがとうございました。
+					<div class="my-3">
+						<span id="time" style="font-size: small;"></span>
+					</div>
+				</h1>
+
+				<?php if ($is_show_present_message && $is_survey_state == NOW && $present_state == "1") : ?>
+					<div class="info py-2">
+						<p>「コンソメたまねぎ棒（２本セット）」をプレゼントしますので</p>
+						<p>この画面をインフォメーションの係員に提示してください。</p>
+						<p class="mt-4">この画面を閉じずに提示してください。</p>
+					</div>
+					<input class="d-block mx-auto" type="button" onclick="reloadToHref()" value="プレゼントを受け取りました。">
+				<?php else : ?>
+					<div class="info py-2">
+						<p>今後とも本四道路のご利用をお願いいたします。</p>
+					</div>
+					<input class="d-block mx-auto" type="button" onclick="reloadToHref()" value="この画面を閉じる">
+				<?php endif; ?>
+			</div>
+			<div id="second">
+				<h1 class="p-4">大変申し訳ございません。<br>本ページの再表示は、行えません。</h1>
+			</div>
+		<?php else : ?>
+			<h1 class="p-4">大変申し訳ありません。<br>本ページの再表示は、行えません。</h1>
+		<?php endif; ?>
+	</div>
+	<div class="mt-5 text-center">
+        <a href="./privacy.html" target="_blank" class="privacy">プライバシーポリシーについて</a>
+	</div>
+</body>
+
+</html>
